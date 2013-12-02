d6a06157d753717ca7b401cdab4033079e5f0bdf
d.fuchiwaki <d.fuchiwaki@everyleaf.com>
2013-12-03 00:52:09 +0900

    first commit

---
+++ mail_form/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'cc147b46514aa3d33fa99aa19303d95560b54fdc8736885d7938a10a870c419df18f0d6a39627d8e57efeb5d7ee808525ed7328bc235c7161a1d6496463216bc'
---
+++ pdf_renderer/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'b9474ec8f3d4b4776f5696e5434ceae59b66c758b48a1b19f49fa8b0392ea68cfdda19d62665821d586002f921b299ae485040021a1b02023e9de5c6b96c699e'
